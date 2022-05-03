184a974b586990096269e716f5f9713ea2ba6555
denchugo <tnkstr@live.jp>
2022-05-03 17:08:51 +0900

    master

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ let eqcode = 'TTL_MTTL'; //地震コード
 let format = 'geojson'; // 出力形式
 let epsg = 4326; //測地系
 let attr = 'T30_I50_PS' //30年間で震度5強以上となる確率
-// let mesh;
 
 let map = L.map("map");
 map.setView([lat, lon], zoom);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-map.on('click', onMapClick);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+	attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+	}).addTo(map);
+// L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
+// 	attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+// 	}).addTo(map);
+	map.on('click', onMapClick);
 
 function onMapClick(e) {
 	let lat = e.latlng.lat;	//クリックされた経度
